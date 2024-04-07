3e321e6c21c61595c58d4f82ec41c35b9ba0683b
suinKim <developerkimmy@gmail.com>
2024-04-07 13:00:43 +0900

    Initial commit

---
+++ src/main/java/com/suin_kim/first_boot_project/FirstBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.suin_kim.first_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstBootProjectApplication.class, args);
+    }
+
+}
