2d8024f7031ba3585414d6fbdecedb5cba91bed3
suinKim <developerkimmy@gmail.com>
2024-04-07 14:24:46 +0900

    pacman ready

--- src/main/java/com/suin_kim/first_boot_project/AppGamingBasicJava.java
+++ src/main/java/com/suin_kim/first_boot_project/AppGamingBasicJava.java
@@ -2,6 +2,7 @@ package com.suin_kim.first_boot_project;
 
 import com.suin_kim.first_boot_project.game.GameRunner;
 import com.suin_kim.first_boot_project.game.MarioGame;
+import com.suin_kim.first_boot_project.game.Pacman;
 import com.suin_kim.first_boot_project.game.SuperContraGame;
 
 public class AppGamingBasicJava {
@@ -9,8 +10,9 @@ public class AppGamingBasicJava {
         // var: allows user to declare variable without specifying its type
         // to import class, press alt and enter (alt + enter)
 //        var marioGame = new MarioGame();
-        var game = new SuperContraGame();
+//        var game = new SuperContraGame();
 //        var game = new MarioGame();
+        var game = new Pacman();
         var gameRunner = new GameRunner(game);
 
         // to create a method, mouseOver where its needed and click the button to create the method.
---
+++ src/main/java/com/suin_kim/first_boot_project/game/Pacman.java
@@ -0,0 +1,16 @@
+package com.suin_kim.first_boot_project.game;
+
+public class Pacman implements GamingConsole{
+    public void up(){
+        System.out.println("Up");
+    }
+    public void down(){
+        System.out.println("Down");
+    }
+    public void left(){
+        System.out.println("Go left");
+    }
+    public void right(){
+        System.out.println("Go right");
+    }
+}
